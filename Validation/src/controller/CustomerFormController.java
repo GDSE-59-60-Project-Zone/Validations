2a93d502b1d9d153d8db281dddef380f0bfe6d3c
sanuv9683 <sanuv9683@gmail.com>
2022-03-10 10:14:03 +0530

    Content Updated

--- Validation/src/controller/CustomerFormController.java
+++ Validation/src/controller/CustomerFormController.java
@@ -13,6 +13,8 @@ import javafx.scene.layout.HBox;
 import view.tdm.CustomerTM;
 
 import java.net.URL;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.ResourceBundle;
 import java.util.regex.Pattern;
 
@@ -27,9 +29,11 @@ public class CustomerFormController implements Initializable {
     public TextField txtCusSalary;
     public TableView<CustomerTM> tblCustomer;
     public JFXButton btnSaveCustomer;
+    LinkedHashMap<TextField, Pattern> map = new LinkedHashMap<>();
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+
         // Initializing table columns with TableTM Properties
         tblCustomer.getColumns().get(0).setCellValueFactory(new PropertyValueFactory<>("id"));
         tblCustomer.getColumns().get(1).setCellValueFactory(new PropertyValueFactory<>("name"));
@@ -50,6 +54,20 @@ public class CustomerFormController implements Initializable {
         });
 
         btnSaveCustomer.setDisable(true);
+
+
+        //add pattern and text to the map
+        //Create a pattern and compile it to use
+        Pattern idPattern = Pattern.compile("^(C00-)[0-9]{3,5}$");
+        Pattern namePattern = Pattern.compile("^[A-z ]{3,15}$");
+        Pattern addressPattern = Pattern.compile("^[A-z0-9 ,/]{4,20}$");
+        Pattern salaryPattern = Pattern.compile("^[1-9][0-9]*(.[0-9]{2})?$");
+
+        map.put(txtCusID,idPattern);
+        map.put(txtCusName,namePattern);
+        map.put(txtCusAddress,addressPattern);
+        map.put(txtCusSalary,salaryPattern);
+
     }
 
     private void saveCustomer() {
@@ -88,76 +106,91 @@ public class CustomerFormController implements Initializable {
     }
 
     private Object validate() {
-        //Id Validation
-        //C00-001
-        //01. C00- this prefix is a must
-        //02. after C00- only numbers allowed. minimum 3 (0-9) maximum 5
-        //reg-ex ^(C00-)[0-9]{3,5}$
-
-        //Name Validate
-        //01. A-z any letter, Spaces are allowed
-        //02. length min 3 max 15
-
-        //Address Validate
-        //01. A-z & 0-9 , /
-        //02. Length min 4 max20
-
-        //Salary
-        //01. first number - 1-9
-        //02. second 0-9 - o or more
-        //03. (.34) if there is a decimal point there should have two numbers after the decimal point
-
-
-        //Create a pattern and compile it to use
-        Pattern idPattern = Pattern.compile("^(C00-)[0-9]{3,5}$");
-        Pattern namePattern = Pattern.compile("^[A-z ]{3,15}$");
-        Pattern addressPattern = Pattern.compile("^[A-z0-9 ,/]{4,20}$");
-        Pattern salaryPattern = Pattern.compile("^[1-9][0-9]*(.[0-9]{2})?$");
-
-
-        //get the type value
-        if (!idPattern.matcher(txtCusID.getText()).matches()) {
-            //if the input is not matching
-            addError(txtCusID);
-            return txtCusID;
-        } else {
-            //if the input is matching
-            removeError(txtCusID);
-
-            //if the Customer ID is ok.. then check the Customer Name
-            if (!namePattern.matcher(txtCusName.getText()).matches()) {
+        for (TextField key : map.keySet()) {
+            Pattern pattern = map.get(key);
+            if (!pattern.matcher(key.getText()).matches()){
                 //if the input is not matching
-                addError(txtCusName);
-                return txtCusName;
-            } else {
-                //if the input is matching
-                removeError(txtCusName);
-
-                //if the Customer Name is ok.. then check the Customer Address
-                if (!addressPattern.matcher(txtCusAddress.getText()).matches()) {
-                    //if the input is not matching
-                    addError(txtCusAddress);
-                    return txtCusAddress;
-
-                } else {
-                    //if the input is matching
-                    removeError(txtCusAddress);
-
-                    //if the Customer Address is ok.. then check the Customer Salary
-                    if (!salaryPattern.matcher(txtCusSalary.getText()).matches()) {
-                        //if the input is not matching
-                        addError(txtCusSalary);
-                        return txtCusSalary;
-                    } else {
-                        //if the input is matching
-                        removeError(txtCusSalary);
-
-                    }
-                }
+                addError(key);
+                return key;
             }
+            removeError(key);
+
         }
         return true;
     }
+
+//    private Object validate() {
+//        //Id Validation
+//        //C00-001
+//        //01. C00- this prefix is a must
+//        //02. after C00- only numbers allowed. minimum 3 (0-9) maximum 5
+//        //reg-ex ^(C00-)[0-9]{3,5}$
+//
+//        //Name Validate
+//        //01. A-z any letter, Spaces are allowed
+//        //02. length min 3 max 15
+//
+//        //Address Validate
+//        //01. A-z & 0-9 , /
+//        //02. Length min 4 max20
+//
+//        //Salary
+//        //01. first number - 1-9
+//        //02. second 0-9 - o or more
+//        //03. (.34) if there is a decimal point there should have two numbers after the decimal point
+//        //Create a pattern and compile it to use
+//        Pattern idPattern = Pattern.compile("^(C00-)[0-9]{3,5}$");
+//        Pattern namePattern = Pattern.compile("^[A-z ]{3,15}$");
+//        Pattern addressPattern = Pattern.compile("^[A-z0-9 ,/]{4,20}$");
+//        Pattern salaryPattern = Pattern.compile("^[1-9][0-9]*(.[0-9]{2})?$");
+//
+//
+//        //get the type value
+//        if (!idPattern.matcher(txtCusID.getText()).matches()) {
+//            //if the input is not matching
+//            addError(txtCusID);
+//            return txtCusID;
+//        } else {
+//            //if the input is matching
+//            removeError(txtCusID);
+//
+//            //if the Customer ID is ok.. then check the Customer Name
+//            if (!namePattern.matcher(txtCusName.getText()).matches()) {
+//                //if the input is not matching
+//                addError(txtCusName);
+//                return txtCusName;
+//            } else {
+//                //if the input is matching
+//                removeError(txtCusName);
+//
+//                //if the Customer Name is ok.. then check the Customer Address
+//                if (!addressPattern.matcher(txtCusAddress.getText()).matches()) {
+//                    //if the input is not matching
+//                    addError(txtCusAddress);
+//                    return txtCusAddress;
+//
+//                } else {
+//                    //if the input is matching
+//                    removeError(txtCusAddress);
+//
+//                    //if the Customer Address is ok.. then check the Customer Salary
+//                    if (!salaryPattern.matcher(txtCusSalary.getText()).matches()) {
+//                        //if the input is not matching
+//                        addError(txtCusSalary);
+//                        return txtCusSalary;
+//                    } else {
+//                        //if the input is matching
+//                        removeError(txtCusSalary);
+//
+//                    }
+//                }
+//            }
+//        }
+//        return true;
+//    }
+
+
+
 
     private void removeError(TextField txtField) {
         txtField.getParent().setStyle("-fx-border-color: green");
