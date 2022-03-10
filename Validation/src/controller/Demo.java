55dfb6c653de9dea4ae40af56ff949780b997d98
sanuv9683 <sanuv9683@gmail.com>
2022-03-10 09:48:37 +0530

    Updated with the comments

--- Validation/src/controller/Demo.java
+++ Validation/src/controller/Demo.java
@@ -10,19 +10,23 @@ public class Demo {
     public static void main(String[] args) {
 
         HashMap<String, Integer> map = new HashMap<>();
+        //how to save a value in a map
+        //first arguement = Key
+        //second arguement = Value
         map.put("A",100);
         map.put("B",200);
         map.put("C",300);
         map.put("D",400);
 
-
+        //how to get a value from the map using the key
         Integer a = map.get("B");
         System.out.println(a);
 
+        //how to get all keys of the map
         map.keySet();//all keys of the map
 
 
-        //how to get all the key set of the map
+        //how to print all the key set of the map
         for (String s : map.keySet()) {
             System.out.println(s);
         }
